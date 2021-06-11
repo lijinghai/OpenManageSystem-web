76a1470a797f3bb556a6ded9a6435fc69ab2adda
Dorian1015 <lijinghailjh@163.com>
2021-06-11 21:34:37 +0800

    2021 6 11 首页优化

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -6,6 +6,8 @@
 <template>
   <div>
     <el-card shadow="always" body-style="padding: 5px 20px;">
+      <el-tag type="success" style="width: 100%;">Dorian</el-tag>
+      <p>月光还是少年的月光，九州一色还是少年的月光</p>
       <el-tag type="success" style="width: 100%;">项目简介</el-tag>
       <p>这是我的大二(2021.6.3)全栈班的后端业务领域课的实训===>东软开放平台的后台管理界面</p>
       <p>东软开放平台的后台管理，完成于本人大二期间（2021年 6月 3日），采用vue与springboot架构开发，前后端完全分离，json格式数据交互，全部由本人独立开发完成。将来将部署在本人的学生版阿里云centos7服务器上。</p>
@@ -16,11 +18,12 @@
       <p>mysql数据库，mybatis(通用Mapper)、实现了分页功能</p>
       <p>springBoot(spring、springMVC)</p>
 <!--      <p>springCloud(eureka、zuul、feign)搭建微服务工程</p>-->
+      <p>集成Swagger2实现简化调试</p>
+      <p>使用redis实现登陆共享与登陆拦截</p>
+      <p>使用AspectJ实现日志的自动添加</p>
       <p>nginx反向代理域名</p>
       <p>采用JWT+MD5加密生成token，写入cookie的方式做无状态登录</p>
       <p>开发工具：webstorm、maven、idea、postman、swagger、git、navicat等</p>
-      <el-tag type="success" style="width: 100%;">Dorian</el-tag>
-      <p>月光还是少年的月光，九州一色还是少年的月光</p>
     </el-card>
   </div>
 </template>
